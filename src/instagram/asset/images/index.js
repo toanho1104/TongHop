664236f9a45b1abb1aadac8e9623aa03c3697b8c
Toan Ho <toanho1104@gmail.com>
2020-10-16 10:10:14 +0700

    add-instagram

---
+++ src/instagram/asset/images/index.js
@@ -0,0 +1,9 @@
+const anhNen=require('./Rectangle8.png')
+const thongBao=require('./Union.png')
+const pr=require('./Group5.png')
+const daTa=require('./Group6.png')
+const chat=require('./Chat.png')
+const back=require('./back.png')
+export{
+    anhNen,thongBao,pr,daTa,chat,back
+}
